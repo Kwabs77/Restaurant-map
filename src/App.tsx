d4507f44c17be3a91a11a72dc4d82b9f9c4ba99d
Kwabena Owusu Achiaw <kwabena.owusu-achiaw@amalitech.org>
2023-05-17 04:47:42 +0000

    basename add

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import SignUp from "./pages/SignUp";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/Restaurant-map">
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/home" element={<Home />} />
